19244b90ec13337b7dc8a231edf25033fc58e86b
artgon93 <artgon93@gmail.com>
2017-06-27 16:15:14 -0600

    Intento de segunda pregunta

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -12,13 +12,24 @@ package tarea2;
 public class Tarea2 {
 
     
-public static String invertir (String str){
-    String resp= "";
-    for(int i = str.length()-1; i>=0;i--){
-        resp = resp + str.charAt(i);
+    public static String invertir (String cadena){
+        String resp= "";
+        for(int i = cadena.length()-1; i>=0;i--){
+            resp = resp + cadena.charAt(i);
+        }
+        return resp;
     }
-    return resp;
-}
+    
+    public static int posicion(String palabra, char letra){
+        int resp=0;
+        for(int i = palabra.length()-1; i>=0;i--){
+            char letraP = palabra.charAt(i);
+            if(letra == letraP)){
+                return i;
+            }
+        }
+        return resp;
+    }
 
     public static void main(String[] args) {
         // TODO code application logic here

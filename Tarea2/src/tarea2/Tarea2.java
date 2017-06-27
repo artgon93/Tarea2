b7cb7db61d51d7f4f9d038080f8b038349518474
artgon93 <artgon93@gmail.com>
2017-06-27 17:36:34 -0600

    Tercer ejercicio resuelto

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -31,10 +31,27 @@ public class Tarea2 {
         return resp;
     }
 
+    public static int comparacion(String palabra1, String palabra2){
+        int resp = 1;
+        if(palabra1.length()==palabra2.length()){ 
+            for (int i =0; i<=palabra1.length()-1;i++){
+                char letra1 = palabra1.charAt(i);
+                char letra2 = palabra2.charAt(i);
+                if(letra1!=letra2){
+                    resp = 0;
+                }            
+            }
+        }else{
+            resp=0;
+        }
+        return resp;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         //System.out.println("casa");
-        System.out.println(posicion("casa",'s'));
+        //System.out.println(posicion("casa",'s'));
+        System.out.println(comparacion("casa","cassa"));
     }
     
 }
